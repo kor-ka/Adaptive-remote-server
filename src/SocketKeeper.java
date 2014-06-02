0e22d0b111dabe0c6c1b2eff0e97896cae8f7f12
Gleb <badgrib@gmail.com>
2014-06-02 19:38:50 +0400

    qr

--- src/SocketKeeper.java
+++ src/SocketKeeper.java
@@ -1,12 +1,22 @@
-import java.net.*;
-import java.util.Queue;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 public class SocketKeeper {
 	  static int port;
    public static void main(String[] ar)    {
 	  
        try {
-    	   System.out.println("Host IP: "+InetAddress.getLocalHost().getHostAddress());
+    	  
     	     System.out.println("Gimme a POOORT!");
     	     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
     	     String portLine = null;
@@ -14,6 +24,7 @@ public class SocketKeeper {
     	 portLine = keyboard.readLine();
     	 try {
 				port = Integer.parseInt(portLine);
+				
 			} catch (Exception e) {
 				
 			}
@@ -23,10 +34,12 @@ public class SocketKeeper {
     		 portLine = keyboard.readLine();    
     		 try {
 				port = Integer.parseInt(portLine);
+				
 			} catch (Exception e) {
 				
 			}
     	 }
+    	 writeAdress(port);
     	 ServerSocket ss = new ServerSocket(port); 
     	
     	 Qqueue q = new Qqueue();
@@ -54,4 +67,52 @@ public class SocketKeeper {
     	
       } catch(Exception e) { e.printStackTrace(); }
    }
+   
+   //UTIL
+   public static void writeAdress(int port){
+	   
+	   try {
+		   
+		   
+		   
+		System.out.println("Host IP+Port: "+InetAddress.getLocalHost().getHostAddress()+":"+port);
+	
+		
+		BitMatrix matrix = new MultiFormatWriter().encode(InetAddress.getLocalHost().getHostAddress()+":"+port, BarcodeFormat.QR_CODE, 20, 20);
+		
+		JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(100,150);
+        
+        //create a label for a heart shape
+        JLabel labelSqare = new JLabel("\u25A0"+"\u0020"+"\u25A0"+"\u2386"+"\u25A0");  
+        
+        frame.add(labelSqare);
+        frame.setVisible(true);
+		
+		for (int h = 0; h<matrix.getHeight(); h++){
+			for(int w = 0; w<matrix.getWidth(); w++){
+				if(matrix.get(h, w)){
+					System.out.print("||");
+					
+				}else{
+					
+					
+					System.out.print("  ");
+				}
+				if(w==matrix.getWidth()-1){
+					System.out.println("");
+				}
+			}
+			
+		}
+	   } catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (WriterException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+   }
+	   
 }
