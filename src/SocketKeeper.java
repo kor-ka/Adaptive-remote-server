daefbfe9bba5766324412d988f10b2e73aafadde
kor_ka <badgrib@gmail.com>
2014-12-23 16:12:28 +0300

    add port lib

--- src/SocketKeeper.java
+++ src/SocketKeeper.java
@@ -1,103 +1,137 @@
 import java.awt.Color;
-        import java.awt.Graphics2D;
-        import java.awt.image.BufferedImage;
-        import java.io.*;
-        import java.net.*;
-        import java.nio.file.Files;
-        import java.nio.file.StandardCopyOption;
-        import java.util.Enumeration;
-        import java.util.Hashtable;
-        import java.util.Scanner;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Scanner;
 
         import javax.swing.Icon;
-        import javax.swing.ImageIcon;
-        import javax.swing.JFrame;
-        import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import jssc.SerialPortList;
 
         import com.google.zxing.BarcodeFormat;
-        import com.google.zxing.EncodeHintType;
-        import com.google.zxing.WriterException;
-        import com.google.zxing.common.BitMatrix;
-        import com.google.zxing.qrcode.QRCodeWriter;
-        import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-        import com.sun.jna.platform.FileUtils;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.sun.jna.platform.FileUtils;
 
 public class SocketKeeper {
     static int port;
     public static void main(String[] ar) throws IOException {
         ServerSocket ss;
         BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+       
+      
+        
+        if (ar[0]==null) {
+			if (OSValidator.isWindows()) {
+				String sysdir = System.getProperty("user.home");
+				String nircmdcPath = sysdir + "/nircmdc.exe";
+				File nircmdc = new File(nircmdcPath);
+				if (!nircmdc.exists()) {
+					nircmdc.createNewFile();
+					InputStream in = SocketKeeper.class
+							.getResourceAsStream("res/nircmdc.exe");
 
-        if(OSValidator.isWindows()){
-            String sysdir = System.getProperty("user.home");
-            String nircmdcPath = sysdir + "/nircmdc.exe";
-            File nircmdc = new File(nircmdcPath);
-            if(!nircmdc.exists()){
-                nircmdc.createNewFile();
-                InputStream in = SocketKeeper.class.getResourceAsStream("res/nircmdc.exe");
+					OutputStream outStream = new FileOutputStream(nircmdc);
 
-                OutputStream outStream = new FileOutputStream(nircmdc);
+					byte[] buffer = new byte[8 * 1024];
+					int bytesRead;
+					while ((bytesRead = in.read(buffer)) != -1) {
+						outStream.write(buffer, 0, bytesRead);
+					}
+					in.close();
+					outStream.close();
+				}
+			}
+			try {
 
-                byte[] buffer = new byte[8 * 1024];
-                int bytesRead;
-                while ((bytesRead = in.read(buffer)) != -1) {
-                    outStream.write(buffer, 0, bytesRead);
-                }
-                in.close();
-                outStream.close();
-            }
-        }
+				port = 1025;
+				while (true) {
 
-        try {
+					try {
+						port += 1;//(int)(Math.random() * ((65535 - 1025) + 1));
+						if (port > 1031)
+							port = 1025;
+						ss = new ServerSocket(port);
+						ss.setReuseAddress(true);
+						new Thread(new Runnable() {
+							public void run() {
+								writeAdress(port);
+								return;
+							}
+						}).start();
 
-            port = 1025;
-            while(true){
+						break;
+					} catch (Exception e) {
 
-                try {
-                    port += 1;//(int)(Math.random() * ((65535 - 1025) + 1));
-                    if(port > 1031)port = 1025;
-                    ss = new ServerSocket(port);
-                    ss.setReuseAddress(true);
-                    new Thread(new Runnable(){
-                        public void run (){
-                            writeAdress(port);
-                            return;
-                        }
-                    }).start();
+					}
+				}
 
-                    break;
-                } catch (Exception e) {
+				Qqueue q = new Qqueue();
+				Results r = new Results();
 
-                }
-            }
+				final Counter cntr = new Counter(q, r);
+				new Thread(cntr).start();
 
+				final ListenServer ls = new ListenServer(ss, q, r);
+				new Thread(ls).start();
+				while (true) {
+					final String say = keyboard.readLine();
+					if (say != null) {
 
+						writeAdress(port);
 
-            Qqueue q = new Qqueue();
-            Results r = new Results();
+						new Thread(new Runnable() {
+							public void run() {
+								ls.say(say);
+								return;
+							}
+						}).start();
+					}
 
-            final Counter cntr = new Counter(q,r);
-            new Thread(cntr).start();
+				}
 
-            final ListenServer ls = new ListenServer(ss, q, r);
-            new Thread(ls).start();
-            while(true){
-                final String say = keyboard.readLine();
-                if(say !=null){
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else{
+			
+			switch (stringToCommand(ar[0])) {
+			case PORT_NAMES:
+				 System.out.println("Prt nms");
+				  String[] portNames = SerialPortList.getPortNames();
+			        for(int i = 0; i < portNames.length; i++){
+			            System.out.println(portNames[i]);
+			        }
+				break;
 
-                    writeAdress(port);
-
-                    new Thread(new Runnable(){
-                        public void run (){
-                            ls.say(say);
-                            return;
-                        }
-                    }).start();
-                }
-
-            }
-
-        } catch(Exception e) { e.printStackTrace(); }
+			case WAT:
+				System.out.println("Hm.");
+				break;
+			}
+			
+			
+		}
+    }
+    
+   
+    public static final int PORT_NAMES = 1;
+    public static final int WAT = 0;
+    
+    static int stringToCommand(String s){
+		if(s.equals("pn")) return PORT_NAMES;
+		return WAT;
+    	
     }
 
     //UTIL
